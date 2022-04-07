2216ed7b143a3dfec2e362a08d075d01e9a4e3bf
VINICIUS LIMA DE SANTANA <vinicius.lsantana2@senacsp.edu.br>
2022-04-06 22:20:41 -0300

    nao esta pronto softdelete

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -15,4 +15,9 @@ class Product extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function Brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
 }
---
+++ database/migrations/2022_04_07_011517_edit_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+    //     schema::table('tags', function (Blueprint $table){ nao esta pronto
+    //         $table->softDeletes();
+    //     });
+    // }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // schema::table('tags', function (Blueprint $table){
+        //     $table->dropColumn('deleted_at');
+
+        // });
+    }
+};

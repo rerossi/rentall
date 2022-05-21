f1bb6a05fd83c59f39dfbcfca6f0cbac8b571711
Vini Black <viniibl4ck@gmail.com>
2022-05-21 16:26:18 -0300

    separando o header e o footer

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,108 +1,43 @@
-<?php
-    // include 'header.php';
-?>
-{{--
-    <link rel="stylesheet" href="../css/login.css">
-
-        <main class ="container">
-
-            <h2> Login Rentall </h2>
-            <form method = "post" action="../login/login.php" >
-                <div class="input-field">
-                    <input type="text" name="email" id="email" placeholder="usuario@exemplo.com">
-                    <div class="underline"> </div>
-                </div>
-                <div class="input-field">
-                    <input type="password" name="password" id="password" placeholder="Senha">
-                    <div class="underline"> </div>
-                </div>
-                <input type="submit" value="continue">
-            </form>
-
-            <div class="footer">
-                <span> Acesse com as midias sociais </span>
-                <div class="social-fields">
-                    <div class="social-field google">
-                        <a href="#">
-                            <i class="fab fa-google"></i>
-                            Entre com Google
-                        </a>
-                    </div>
-
-                    <div class="social-field facebook">
-                        <a href="#">
-                            <i class="fab fa-facebook-f"></i>
-                            Entre com Facebook
-                        </a>
-                    </div>
-
-                    <span class="novo-usuario">
-                        <a href="novoUser.php">
-                         Cadastre-se
-                        </a>
-                        </span>
-                </div>
+@extends('layouts.header')
+@section('content')
+<link rel="stylesheet" href="../css/login.css">
+    <main class="container">
+        <h2> Login Rentall </h2>
+        <form method="post" action="../login/login.php">
+            <div class="input-field">
+                <input type="text" name="email" id="email" placeholder="usuario@exemplo.com">
+                <div class="underline"> </div>
             </div>
-
-        </main>
-</section>
-</html> --}}
-
-
-
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
-
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <div class="input-field">
+                <input type="password" name="password" id="password" placeholder="Senha">
+                <div class="underline"> </div>
             </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
-
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
+            <input type="submit" value="continue">
         </form>
-    </x-auth-card>
-</x-guest-layout>
+
+        <div class="footer">
+            <span> Acesse com as midias sociais </span>
+            <div class="social-fields">
+                <div class="social-field google">
+                    <a href="#">
+                        <i class="fab fa-google"></i>
+                        Entre com Google
+                    </a>
+                </div>
+
+                <div class="social-field facebook">
+                    <a href="#">
+                        <i class="fab fa-facebook-f"></i>
+                        Entre com Facebook
+                    </a>
+                </div>
+
+                <span class="novo-usuario">
+                    <a href="novoUser.php">
+                        Cadastre-se
+                    </a>
+                </span>
+            </div>
+        </div>
+    </main>
+@endsection
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,85 @@
+@section('navbar')
+    <header>
+        <nav class="navbar navbar-light " style="background-color:rgb(20, 124, 162);">
+            <div class="container-fluid">
+                <div class="row w-100">
+                    <div class="col-2">
+                        <a class="navbar-brand" href="#">
+                            logo rentall
+                            <img src="../imagens/logo-completo-branco.png" alt="" width="150px"
+                                class="d-inline-block align-text-center">
+                        </a>
+                    </div>
+
+                    <div class="col-8">
+                        <div class="justify-content-center mt-2 row" style="margin-left: 70px;">
+                            <form class="d-flex col-8">
+                                <input class="form-control me-2" style="min-width: auto; " type="search" placeholder="Search"
+                                    aria-label="Search">
+                                <button class="btn btn-outline"
+                                    style="color:rgb(20, 124, 162); background-color: white; border-color:white;"
+                                    type="submit">Search</button>
+                            </form>
+                        </div>
+                    </div>
+
+                    <div class="col-2 text-end">
+
+
+
+                        @if (Route::has('login'))
+                            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                                @auth
+                                    <span style="text-decoration: none; color: white; font-weight:lighter"> Olá, pessoa
+                                    </span>
+                                    <a href="{{ url('/dashboard') }}"
+                                        class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                                @else
+                                    <a href="{{ route('login') }}"
+                                        class="text-sm text-gray-700 dark:text-gray-500 underline">Entre</a>
+
+                                    {{-- @if (Route::has('register'))
+                                            <a href="{{ route('register') }}"
+                                                class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                                        @endif --}}
+                                @endauth
+                            </div>
+                        @endif
+
+
+                    </div>
+                </div>
+
+                <div class="row w-100">
+                    <div class="col-12">
+                        <ul class="nav justify-content-center">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+                                    aria-expanded="false" style="color:white; font-weight:lighter" id="navbarDropdown">
+                                    Categorias
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Esportes</a></li>
+                                    <li><a class="dropdown-item" href="#">Games</a></li>
+                                    <li><a class="dropdown-item" href="#">Eletroportateis</a></li>
+                                    <li><a class="dropdown-item" href="#">Ferramentas</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#" style="color:white; font-weight:lighter">Catálogo</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#" style="color:white; font-weight:lighter"> Meus
+                                    Produtos </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#" style="color:white; font-weight:lighter">Cadastrar
+                                    produto</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+@endsection

52fa0e82de4867fbd3cdcf4c257f3edf6a9e5739
Rodrigo Rossi <rerossi1985@gmail.com>
2022-06-04 16:22:33 -0300

    ajustando ultima aula

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,11 +4,61 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Cart;
+use App\Models\Product;
 
 class CartController extends Controller
 {
     public function index(){
         $itens = Cart::where('user_id', Auth()->user()->id)->get();
         return view('cart.index')->with('itens', $itens);
+    }
+
+    public function store(Product $product){
+        $user = auth()->user();
+
+        $cart = Cart::where([
+            'user_id' => $user->id,
+            'product_id' => $product->id])->first();
+
+            //Se o produto já estiver no carrinho
+            if($cart){
+                $cart->update([
+                    'units' =>$cart->units + 1
+                ]);
+            }else{
+                $cart = Cart::create([
+                    'user_id' => $user->id,
+                    'product_id' => $product->id,
+                    'units' => 1
+                ]);
+            }
+        session()->flash('success', 'O produto ('.$product->name.') foi adicionado.');
+        return redirect()->back();
+    }
+
+    public function destroy(Product $product){
+        $user = auth()->user();
+
+        $cart = Cart::where([
+            'user_id' => $user->id,
+            'product_id' => $product->id])->first();
+
+            //Se o produto já estiver no carrinho
+            if(!$cart){
+                session()->flash('error', 'O produto ('.$product->name.') não está no carrinho.');
+                return redirect()->back();
+
+            }
+
+            if($cart->units > 1){
+                $cart->update([
+                    'units' =>$cart->units - 1
+                ]);
+            }else{
+                $cart->delete();
+
+            }
+        session()->flash('success', 'O produto ('.$product->name.') foi removido do carrinho.');
+        return redirect()->back();
     }
 }
--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -1,25 +1,69 @@
-@extends('layouts.app')
+@extends('layouts.store')
 @section('content')
-
-
 <section class="container mt-5">
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Produto</th>
-                <th>Quantidade</th>
-                <th>Preço</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($itens as $item)
-            <tr>
-                <td>{{$item->Product->name}}</td>
-                <td>{{$item->units}}</td>
-                <td>{{$item->Product->price}}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="row">
+        <div class="col-4">
+            <h2>Detalhes do Endereço</h2>
+            <form action="{{ route('order.store') }}" method="POST">
+                @csrf
+                <div class="form-group">
+                    <label for="zipcode">CEP</label>
+                    <input type="text" class="form-control" name="zipcode" placeholder="CEP">
+                </div>
+                <div class="form-group">
+                    <label for="address">Endereco</label>
+                    <input type="text" class="form-control" name="address" placeholder="Endereço">
+                </div>
+                <div class="form-group">
+                    <label for="city">Cidade</label>
+                    <input type="text" class="form-control" name="city" placeholder="Cidade">
+                </div>
+                <div class="form-group">
+                    <label for="state">Estado</label>
+                    <input type="text" class="form-control" name="state" placeholder="Estado">
+                </div>
+                <button type="submit" class="btn btn-lg btn-primary my-2 float-end">Comprar</button>
+            </form>
+        </div>
+        <div class="col-8">
+            <h2>Detalhes dos Produtos</h2>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Produto</th>
+                        <th>Preço</th>
+                        <th>Quantidade</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php $total = 0.0 ?>
+                @foreach($itens as $item)
+                    <tr>
+                        <td>{{$item->Product->name}}</td>
+                        <td>{{$item->Product->price}}</td>
+                        <td>
+                            <form action="{{ route('cart.store', $item->Product->id) }}" method="POST" style="display:inline">
+                                @csrf
+                                <button type="submit" class="btn btn-primary btn-sm">+</button>
+                            </form>
+                            {{$item->units}}
+                            <form action="{{ route('cart.destroy', $item->Product->id) }}" method="POST" style="display:inline">
+                                @csrf
+                                @method("DELETE")
+                                <button type="submit" class="btn btn-primary btn-sm">-</button>
+                            </form>
+                        </td>
+                    </tr>
+                    <?php $total += $item->Product->price * $item->units ?>
+                @endforeach
+                    <tr>
+                        <td class="fw-bold">Total</td>
+                        <td class="fw-bold">R$ {{ number_format($total,2) }}</td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </section>
 @endsection

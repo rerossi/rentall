6f27b63a60b25789ab0a8fffde127c99760bcb7a
Vini Black <viniibl4ck@gmail.com>
2022-06-05 17:31:24 -0300

    arrumando telas produtos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="pt-br">
 
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Laravel</title>
+  <title>Rentall</title>
 
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
--- resources/views/product/trash.blade.php
+++ resources/views/product/trash.blade.php
@@ -1,46 +1,40 @@
-@extends('layouts.app')
+@extends('layouts.header')
 
 @section('content')
+    <main style="height: 100vh;" class="d-flex flex-column justify-content-center">
+        <section class="container fundo rounded-3" style="background-color: rgb(210, 210, 210)">
+            <h2 class="text-center h2 py-2"> Lixeira Produtos </h2>
+            <table class="table ">
+                <thead>
+                    <tr>
+                        <th>Titulo</th>
+                        <th>Categoria</th>
+                        <th>Preço</th>
+                        <th>Descrição</th>
+                        <th>Imagem</th>
+                        <th>Restaurar</th>
 
-<div class="text-center justify-content-center d-flex align-items-center pb-3 ">
-    <button class="btn botao btn-dark"><a href="../telas/formProduto.php" class="text-decoration-none text-reset"> Cadastrar Produto </a></button>
-</div>
-<section class="container fundo">
-    <h2> Meus Produtos </h2>
-        <table class="table ">
-            <thead>
-                <tr>
-
-                    <th>Titulo</th>
-                    <th>Categoria</th>
-                    <th>Preço</th>
-                    <th>Descrição</th>
-                    <th>Imagem</th>
-                    <th>Restaurar</th>
-
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ( $products as $product )
-                <tr>
-                    {{-- <td class="text-center fs-3" colspan="10"> Sem Resgistros </td> --}}
-
-                           <tr>
-                            <td>{{$product->name}}</td>
-                            <td>{{$product->Category->name}}</td>
-                            <td>{{$product->price}}</td>
-                            <td>{{$product->description}}</td>
-                            <td><img src="{{asset($product->image)}}" style="width:100px;"></td>
-                            <td><button name="editar" class="btn btn-success my-2"> <a href="{{route('product.restore', $product->id)}}">Restaurar</a></button></td>
-
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($products as $product)
+                        <td>{{ $product->name }}</td>
+                        <td>{{ $product->Category->name }}</td>
+                        <td>{{ $product->price }}</td>
+                        <td>{{ $product->description }}</td>
+                        <td><img src="{{ asset($product->image) }}" style="width:100px;"></td>
+                        <td>
+                            <button name="editar" class="btn btn-success my-2">
+                                <a href="{{ route('product.restore', $product->id) }}">
+                                    Restaurar
+                                </a>
+                            </button>
+                        </td>
                         </tr>
+                    @endforeach
 
-                @endforeach
                 </tbody>
-        </table>
-</section>
-
-
-
-
+            </table>
+        </section>
+    </main>
 @endsection

69ea23c8f3934d530b3802adc97001f806d91943
Rodrigo Rossi <rerossi1985@gmail.com>
2022-06-08 19:21:45 -0300

    arrumando o carrinho

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,83 +1,118 @@
 @section('footer')
-    <footer class="text-center text-lg-start text-white" style="background-color: rgb(20, 124, 162)">
-        <!-- Grid container -->
-        <div class="container p-2 pb-0">
-            <!-- Section: Links -->
-            <section class="">
-                <!--Grid row-->
-                <div class="row">
-                    <!-- Grid column -->
-                    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+<link rel="stylesheet" href="style.css">
+<script src="https://kit.fontawesome.com/1ab94d0eba.js" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
 
-                        <h6 class="text-uppercase mb-4 font-weight-bold">
-                            Company name
-                        </h6>
-                        <img src="../imagens/logo-branco.png" style="width: 150px">
+    <div class="container my-2">
+        <!-- Footer -->
+        <footer class="text-center text-lg-start text-white" style="background-color: rgb(20, 124, 162)">
+            <!-- Grid container -->
+            <div class="container p-2 pb-0">
+                <!-- Section: Links -->
+                <section class="">
+                    <!--Grid row-->
+                    <div class="row">
+                        <!-- Grid column -->
+                        <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-4">
+                            
+                            <img src="./imagens/logo-branco.png" style="width: 150px">
+    
+                            <!-- 
+                            <p>
+                                Here you can use rows and columns to organize your footer
+                                content. Lorem ipsum dolor sit amet, consectetur adipisicing
+                                elit.
+                            </p> -->
+                        </div>
+                        <!-- Grid column -->
+    
+                        <hr class="w-100 clearfix d-md-none" />
+    
+                        <!-- Grid column -->
+                        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3 ">
+                            <h6 class="text-uppercase mb-4 font-weight-bold">Conheca a Rentall</h6>
+                            <p class="text-white">
+                                Trabalhe Conosco
+                            </p>
+                            <p class="text-white">
+                                Perguntas Frequentes
+                            </p>
+                            <p class="text-white">
+                                Central de Ajuda
+                            </p>
+                            <p class="text-white">
+                               Sobre a Rentall
+                            </p>
+                        </div>
+                        <!-- Grid column -->
+    
+                        <hr class="w-100 clearfix d-md-none" />
+    
+                        <!-- Grid column -->
+                        <hr class="w-100 clearfix d-md-none" />
+    
+                        <!-- Grid column -->
+                        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                            <h6 class="text-uppercase mb-4 font-weight-bold">Contatos</h6>
+                            <p><i class="fas fa-home mr-3"></i> São Paulo, SP 05787-901, BR</p>
+                            <p><i class="fas fa-envelope mr-3"></i> info@rentall.com</p>
+                            <p><i class="fas fa-phone mr-3"></i> 11 4213-5232</p>
+                            <p><i class="fas fa-print mr-3"></i> 11 96321-5254</p>
+                        </div>
+                        <!-- Grid column -->
+    
+                        <!-- Grid column -->
+                        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                            <h6 class="text-uppercase mb-4 font-weight-bold">Siga-nos</h6>
+    
+                            <!-- Facebook -->
+                            <a class="btn btn-primary btn-floating m-1" style="background-color: #3b5998" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
+    
+                            <!-- Twitter -->
+                            <a class="btn btn-primary btn-floating m-1" style="background-color: #55acee" href="#!" role="button"><i class="fab fa-twitter"></i></a>
+    
+                            <!-- Google -->
+                            <a class="btn btn-primary btn-floating m-1" style="background-color: #dd4b39" href="#!" role="button"><i class="fab fa-google"></i></a>
+    
+                            <!-- Instagram -->
+                            <a class="btn btn-primary btn-floating m-1" style="background-color: #ac2bac" href="#!" role="button"><i class="fab fa-instagram"></i></a>
+    
+                            <!-- Linkedin -->
+                            <a class="btn btn-primary btn-floating m-1" style="background-color: #0082ca" href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
+                            <!-- Github -->
+                            <a class="btn btn-primary btn-floating m-1" style="background-color: #333333" href="#!" role="button"><i class="fab fa-github"></i></a>
+                        </div>
                     </div>
-                    <!-- Grid column -->
-                    <hr class="w-100 clearfix d-md-none" />
-                    <!-- Grid column -->
-                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                        <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
-                        <p>
-                            <a class="text-white">MDBootstrap</a>
-                        </p>
-                        <p>
-                            <a class="text-white">MDWordPress</a>
-                        </p>
-                        <p>
-                            <a class="text-white">BrandFlow</a>
-                        </p>
-                        <p>
-                            <a class="text-white">Bootstrap Angular</a>
-                        </p>
-                    </div>
-                    <!-- Grid column -->
-                    <hr class="w-100 clearfix d-md-none" />
-                    <!-- Grid column -->
-                    <hr class="w-100 clearfix d-md-none" />
-                    <!-- Grid column -->
-                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-                        <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
-                        <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-                        <p><i class="fas fa-envelope mr-3"></i> info@gmail.com</p>
-                        <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-                        <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
-                    </div>
-                    <!-- Grid column -->
-                    <!-- Grid column -->
-                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-                        <h6 class="text-uppercase mb-4 font-weight-bold">Follow us</h6>
-                        <!-- Facebook -->
-                        <a class="btn btn-primary btn-floating m-1" style="background-color: #3b5998" href="#!"
-                            role="button"><i class="fab fa-facebook-f"></i></a>
-                        <!-- Twitter -->
-                        <a class="btn btn-primary btn-floating m-1" style="background-color: #55acee" href="#!"
-                            role="button"><i class="fab fa-twitter"></i></a>
-                        <!-- Google -->
-                        <a class="btn btn-primary btn-floating m-1" style="background-color: #dd4b39" href="#!"
-                            role="button"><i class="fab fa-google"></i></a>
-                        <!-- Instagram -->
-                        <a class="btn btn-primary btn-floating m-1" style="background-color: #ac2bac" href="#!"
-                            role="button"><i class="fab fa-instagram"></i></a>
-                        <!-- Linkedin -->
-                        <a class="btn btn-primary btn-floating m-1" style="background-color: #0082ca" href="#!"
-                            role="button"><i class="fab fa-linkedin-in"></i></a>
-                        <!-- Github -->
-                        <a class="btn btn-primary btn-floating m-1" style="background-color: #333333" href="#!"
-                            role="button"><i class="fab fa-github"></i></a>
-                    </div>
-                </div>
-                <!--Grid row-->
-            </section>
-            <!-- Section: Links -->
-        </div>
-        <!-- Grid container -->
-        <!-- Copyright -->
-        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
-            © 2022 Copyright:
-            <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-        </div>
-        <!-- Copyright -->
-    </footer>
+                    <!--Grid row-->
+                </section>
+                <!-- Section: Links -->
+            </div>
+            <!-- Grid container -->
+    
+            <!-- Copyright -->
+            <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+             
+              
+            </div>
+            <!-- Copyright -->
+        </footer>
+        <!-- Footer -->
+    </div>
+    <!-- End of .container -->
+    
+</body>
+
+
+</html>
 @endsection
--- resources/views/layouts/store.blade.php
+++ resources/views/layouts/store.blade.php
@@ -70,25 +70,124 @@
         @endif
         @yield('content')
     </main>
-    <footer class="container p-5 bg-primary text-white">
-        <div class="row">
-            <div class="col-sm-8 mb-5">
-                <h2 class="h4">Localização:</h2>
-                <address>Rua Lorem, ipsum dolor, 386<br>
-                Lorem, ipsum - Lorem, LR<br>
-                CEP: 00000-000<br>
-                Telefone: (11) 8888-8888
-                </address>
+
+
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+<link rel="stylesheet" href="style.css">
+<script src="https://kit.fontawesome.com/1ab94d0eba.js" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="my-2">
+        <!-- Footer -->
+        <footer class="text-center text-lg-start text-white" style="background-color: rgb(20, 124, 162)">
+            <!-- Grid container -->
+            <div class="container p-2 pb-0">
+                <!-- Section: Links -->
+                <section class="">
+                    <!--Grid row-->
+                    <div class="row">
+                        <!-- Grid column -->
+                        <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-4">
+                            
+                            <img src="./imagens/logo-branco.png" style="width: 150px">
+    
+                            <!-- 
+                            <p>
+                                Here you can use rows and columns to organize your footer
+                                content. Lorem ipsum dolor sit amet, consectetur adipisicing
+                                elit.
+                            </p> -->
+                        </div>
+                        <!-- Grid column -->
+    
+                        <hr class="w-100 clearfix d-md-none" />
+    
+                        <!-- Grid column -->
+                        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3 ">
+                            <h6 class="text-uppercase mb-4 font-weight-bold">Conheca a Rentall</h6>
+                            <p class="text-white">
+                                Trabalhe Conosco
+                            </p>
+                            <p class="text-white">
+                                Perguntas Frequentes
+                            </p>
+                            <p class="text-white">
+                                Central de Ajuda
+                            </p>
+                            <p class="text-white">
+                               Sobre a Rentall
+                            </p>
+                        </div>
+                        <!-- Grid column -->
+    
+                        <hr class="w-100 clearfix d-md-none" />
+    
+                        <!-- Grid column -->
+                        <hr class="w-100 clearfix d-md-none" />
+    
+                        <!-- Grid column -->
+                        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                            <h6 class="text-uppercase mb-4 font-weight-bold">Contatos</h6>
+                            <p><i class="fas fa-home mr-3"></i> São Paulo, SP 05787-901, BR</p>
+                            <p><i class="fas fa-envelope mr-3"></i> info@rentall.com</p>
+                            <p><i class="fas fa-phone mr-3"></i> 11 4213-5232</p>
+                            <p><i class="fas fa-print mr-3"></i> 11 96321-5254</p>
+                        </div>
+                        <!-- Grid column -->
+    
+                        <!-- Grid column -->
+                        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                            <h6 class="text-uppercase mb-4 font-weight-bold">Siga-nos</h6>
+    
+                            <!-- Facebook -->
+                            <a class="btn btn-primary btn-floating m-1" style="background-color: #3b5998" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
+    
+                            <!-- Twitter -->
+                            <a class="btn btn-primary btn-floating m-1" style="background-color: #55acee" href="#!" role="button"><i class="fab fa-twitter"></i></a>
+    
+                            <!-- Google -->
+                            <a class="btn btn-primary btn-floating m-1" style="background-color: #dd4b39" href="#!" role="button"><i class="fab fa-google"></i></a>
+    
+                            <!-- Instagram -->
+                            <a class="btn btn-primary btn-floating m-1" style="background-color: #ac2bac" href="#!" role="button"><i class="fab fa-instagram"></i></a>
+    
+                            <!-- Linkedin -->
+                            <a class="btn btn-primary btn-floating m-1" style="background-color: #0082ca" href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
+                            <!-- Github -->
+                            <a class="btn btn-primary btn-floating m-1" style="background-color: #333333" href="#!" role="button"><i class="fab fa-github"></i></a>
+                        </div>
+                    </div>
+                    <!--Grid row-->
+                </section>
+                <!-- Section: Links -->
             </div>
-            <div class="col-sm-4">
-                <h2 class="h4">Horario de Funcionamento para loja fisica</h2>
-                <ul class="list-unstyled">
-                    <li>Segunda - Sexta: 9:00 - 20:00</li>
-                    <li>Sábado - 9:00 - 16:00</li>
-                </ul>
+            <!-- Grid container -->
+    
+            <!-- Copyright -->
+            <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+             
+              
             </div>
-        </div>
-    </footer>
+            <!-- Copyright -->
+        </footer>
+        <!-- Footer -->
+    </div>
+    <!-- End of .container -->
+    
+</body>
+
+
+</html>
 </body>
 
 </html>

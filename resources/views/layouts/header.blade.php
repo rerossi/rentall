15a19b2d081febc9a8391f13e2fae744518cbe4c
Rodrigo Rossi <rerossi1985@gmail.com>
2022-06-10 19:48:22 -0300

    atualizando

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,6 +7,7 @@
 
     <title>Rentall</title>
 
+    
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/1ab94d0eba.js" crossorigin="anonymous"></script>
